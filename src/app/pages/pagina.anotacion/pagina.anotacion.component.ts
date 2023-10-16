e39f56fdf4e9f6eeaa7d8827caa86db00de44c39
Eduardo Mendez <dizu.eduardo@gmail.com>
2023-10-15 19:17:47 -0500

    Method to obtain emaildocente from anotaciones

--- src/app/pages/pagina.anotacion/pagina.anotacion.component.ts
+++ src/app/pages/pagina.anotacion/pagina.anotacion.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f5c9673c85960f7affd5659ea426ed6db029c71f8caa0ba68957e56074cbdda
-size 1404
+oid sha256:e46339765ef5f387b05ef781ca5ccc3f7c94a2437bb2f8ba54bacaab7a8e0da3
+size 2425
