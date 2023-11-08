283aa9632d83f2c1629ef17f9b95e73a5cc62120
Eduardo Mendez <dizu.eduardo@gmail.com>
2023-11-07 21:22:13 -0500

    Horarios y solicitar reunión creado

--- src/app/pages/docentes/docentes.component.ts
+++ src/app/pages/docentes/docentes.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30838593d87ba2213c42069cf109af57186dc878fbd17bc40781f47de5e31381
-size 1207
+oid sha256:afe331c1bd22204a639fd48b8892eb7a1526dd4b9e29929d9a90249e9d092f8a
+size 1587
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43daf23fb1df9432403fad1ddefcea7dd94902bad66a988878e232899814ef62
-size 543
+oid sha256:d6568961d7981a04159368effcdf68c06dce38bbe61c8c7197f04f43c35935f9
+size 632
